e8882a0651eac6cee31d42c3c3468bac81df1f93
Nikita <Nkmelnikov@hotmail.com>
2018-10-23 22:58:47 +0300

    solving conflicts

--- resources/views/home/referrals.blade.php
+++ resources/views/home/referrals.blade.php
@@ -17,35 +17,7 @@
                                 <div class="basicBlock__text">{!! trans('home/referrals.refText') !!}
                                 </div>
                                 <div class="basicBlock__bonusText">{!! trans('home/referrals.refBonus') !!} 5.00%</div>
-                                @if(Auth::user()->wallets()->first())
-                                <div class="dataTable">
-                                    <table class="dataTable__list">
-                                        <tr>
-                                            <th>{!! trans('home/referrals.tableRef') !!}</th>
-                                            <th>{!! trans('home/referrals.tableBonus') !!}</th>
-                                        </tr>
-                                        @if(isset($referrals['stat']))
-                                            @foreach($referrals['stat'] as $key => $referral)
-                                                <tr class="dataTable__success">
-                                                    <td>{{ $key }}</td>
-                                                    <td>{{ $referral['token_sum'] }}</td>
-                                                </tr>
-                                                {{--<tr class="dataTable__success">--}}
-                                                {{--<td>jhfcsaiudgfcubryc7b467b32a5x6b4q2</td>--}}
-                                                {{--<td>+230 ETH</td>--}}
-                                                {{--</tr>--}}
-                                            @endforeach
 
-                                        @else
-                                            <tr class="dataTable__error">
-                                                <td colspan="2"><h3>{!! trans('home/referrals.tableNoReferrals') !!}</h3></td>
-                                            </tr>
-                                        @endif
-                                    </table>
-                                </div>
-                                    @else
-                                <h4>{!! trans('home/referrals.tableNoWallet') !!}</h4>
-                                    @endif
                             </div>
                         </div>
                     </div>
