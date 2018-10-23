3f12616ba552ff28c6373b0510df2223019f6ce3
Sergej <serezha11shumov@mail.ru>
2018-10-23 17:54:27 +0300

    change referral checking proccess

--- resources/views/home/referrals.blade.php
+++ resources/views/home/referrals.blade.php
@@ -27,13 +27,17 @@
                         <div class="basicBlock basicBlock">
                             <div class="basicBlock__content">
                                 <form class="loginForm icoForm" action="#">
-                                    <div class="basicBlock__text"> Your referral link</div>
-                                    <div class="formControl">
-                                        <input class="icoForm__input icoForm__input--canCopy" type="text"
-                                               value="{{url('/').'/?ref='.Auth::user()->token}}" name="ethWallet"
-                                               id="ethWallet"><span class="icoForm__copy"
-                                                                    v-on:click="copyToBuffer"> </span>
-                                    </div>
+                                    @if(Auth::user()->wallets()->first())
+                                        <div class="basicBlock__text"> Your referral link</div>
+                                        <div class="formControl">
+                                            <input class="icoForm__input icoForm__input--canCopy" type="text"
+                                                   value="{{url('/').'/?ref='.Auth::user()->token}}" name="ethWallet"
+                                                   id="ethWallet"><span class="icoForm__copy"
+                                                                        v-on:click="copyToBuffer"> </span>
+                                        </div>
+                                    @else
+                                        <h4 class="referral__message">To participate in the referral program, add a wallet</h4>
+                                    @endif
                                 </form>
                             </div>
                         </div>
@@ -57,30 +61,15 @@
 
                         @else
                             <tr>
-                                <td class="referral__cell referral__cell--no-referral">No referrals</td>
+                                <td class="referral__cell referral__cell--person">No</td>
+                                <td class="referral__cell referral__cell--bonus">0</td>
                             </tr>
                         @endif
                     </table>
                 </div>
             @else
-                <h4 class="referral__message">To participate in the referral program, add a wallet</h4>
+
             @endif
-            {{--<div class="referral__table-wrap referral__table-wrap--active">--}}
-                {{--<table class="referral__table">--}}
-                    {{--<tr>--}}
-                        {{--<th class="referral__cell referral__cell--person referral__cell--th">Reffered person</th>--}}
-                        {{--<th class="referral__cell referral__cell--bonus referral__cell--th">Bonus</th>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td class="referral__cell referral__cell--person">jhfcsaiudgfcubryc7b467b32a5x6b4q2</td>--}}
-                        {{--<td class="referral__cell referral__cell--bonus">+230 ETH</td>--}}
-                    {{--</tr>--}}
-                    {{--<tr>--}}
-                        {{--<td class="referral__cell referral__cell--person">jhfcsaiudgfcubryc7b467b32a5x6b4q2</td>--}}
-                        {{--<td class="referral__cell referral__cell--bonus">+230 ETH</td>--}}
-                    {{--</tr>--}}
-                {{--</table>--}}
-            {{--</div>--}}
         </div>
     </div>
 @endsection
