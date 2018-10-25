520c490a8a157711b40f2c52c8e9554b1af7efb7
Sergej <serezha11shumov@mail.ru>
2018-10-25 15:00:37 +0300

    change welcome layout, basic and responsive styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="welcome">
-        <div class="welcome__item welcome__item--registration">
-            <a href="{{ route('register') }}" style="font-size: 25px" class="welcome__link">{!! trans('modals/modals.signUp_title') !!}</a>
-        </div>
-        <div class="welcome__item">
-            <a href="{{ route('login') }}" style="font-size: 25px" class="welcome__link">{!! trans('modals/modals.signIn_title') !!}</a>
-            <br>
-            <a href="{{ route('password.request') }}" style="font-size: 12px" class="welcome__link">{!! trans('home/welcome.forgotPwd_a') !!}</a>
+    <div class="welcome__header">
+        <div class="dropdown">
+            <button class="dropbtn welcome__button-language">Languages</button>
+            <div class="dropdown-content">
+                <a href="#">English</a>
+                <a href="#">Russian</a>
+                <a href="#">Mandarin(Chinise)</a>
+                <a href="#">Spanish</a>
+                <a href="#">Japanese</a>
+            </div>
         </div>
     </div>
+    <section class="welcome">
+        <div class="welcome__wrapper">
+            <div class="welcome__item welcome__item--registration">
+                <a href="{{ route('register') }}" style="font-size: 25px" class="welcome__link">{!! trans('modals/modals.signUp_title') !!}</a>
+            </div>
+            <div class="welcome__item">
+                <a href="{{ route('login') }}" style="font-size: 25px" class="welcome__link">{!! trans('modals/modals.signIn_title') !!}</a>
+                <br>
+                <a href="{{ route('password.request') }}" style="font-size: 12px" class="welcome__link welcome__link--password">{!! trans('home/welcome.forgotPwd_a') !!}</a>
+            </div>
+        </div>
+    </section>
 @endsection
 
 {{--<!doctype html>--}}
