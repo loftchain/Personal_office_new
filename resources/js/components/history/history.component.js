d10e342e69d4a582d4d696f629ab709f63f44c63
Sergej <serezha11shumov@mail.ru>
2018-10-30 16:52:06 +0300

    adding static part of history component, some responsive admin styles

---
+++ resources/js/components/history/history.component.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default {
+  name: 'history',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
