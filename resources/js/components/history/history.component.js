6f6734947d381ed92e7d8a794c9a79eaed99ca7f
Sergej <serezha11shumov@mail.ru>
2018-10-31 15:15:52 +0300

    temporary version of history page

--- resources/js/components/history/history.component.js
+++ resources/js/components/history/history.component.js
@@ -6,21 +6,47 @@ export default {
   props: [],
   data () {
     return {
-      histories: null
+      histories: [],
+      currentSort:'date',
+      currentSortDir:'desc'
     }
   },
-  computed: {
 
-  },
-  mounted () {
+  created() {
     this.getHistories();
   },
-  methods: {
-    async getHistories() {
-      await axios.get('history/get')
-        .then(res => {
-          this.histories = res.data;
+
+  computed: {
+    sortedItems:function() {
+      if(this.histories !== []){
+        return this.histories.sort((a,b) => {
+          let modifier = 1;
+          if(this.currentSortDir === 'desc') modifier = -1;
+          if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+          if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+          return 0;
         });
+      }
+    }
+  },
+
+  mounted() {
+    console.log(this.histories);
+  },
+
+  methods: {
+    getHistories() {
+      axios.get('history/get')
+        .then(res => {
+            this.histories = res.data;
+        });
+    },
+
+    sort(s) {
+      if(s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+      }
+      this.currentSort = s;
     }
   }
 }
