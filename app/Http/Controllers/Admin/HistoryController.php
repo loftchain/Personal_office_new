f42e8a5e6c7b42212891b1c76af3b3e325cb72cb
Alexander <meeek196@gmail.com>
2018-10-31 10:34:37 +0300

    Fixed admin history

--- app/Http/Controllers/Admin/HistoryController.php
+++ app/Http/Controllers/Admin/HistoryController.php
@@ -15,45 +15,8 @@ class HistoryController extends Controller
 
     public function get()
     {
-        $histories = InvestorHistoryFields::with('investor')->get();
+        $histories = InvestorHistoryFields::get();
 
-        $histories = $histories->toArray();
-        $data = [];
-
-        foreach ($histories as $history){
-            $data[$history['investor']['id']] = [];
-            foreach ($history as $key => $action){
-                if($key === 'reg_email'){
-                    if($action){
-                        $data[$history['investor']['id']]['reg'][] = $history['reg_email'];
-                        $data[$history['investor']['id']]['reg'][] = $history['reg_pwd'];
-                        $data[$history['investor']['id']]['reg'][] = $history['reg_at'];
-                    }
-                }
-                if($key === 'change_email_new'){
-                    if($action){
-                        $data[$history['investor']['id']]['change_email'][] = $history['change_email_new'];
-                        $data[$history['investor']['id']]['change_email'][] = $history['change_email_old'];
-                        $data[$history['investor']['id']]['change_email'][] = $history['change_email_at'];
-                    }
-                }
-                if($key === 'change_pwd_new'){
-                    if($action){
-                        $data[$history['investor']['id']]['change_pwd'][] = $history['change_pwd_new'];
-                        $data[$history['investor']['id']]['change_pwd'][] = $history['change_pwd_old'];
-                        $data[$history['investor']['id']]['change_pwd'][] = $history['change_pwd_at'];
-                    }
-                }
-                if($key === 'forgot_pwd_new'){
-                    if($action){
-                        $data[$history['investor']['id']]['forgot_pwd'][] = $history['forgot_pwd_new'];
-                        $data[$history['investor']['id']]['forgot_pwd'][] = $history['forgot_pwd_old'];
-                        $data[$history['investor']['id']]['forgot_pwd'][] = $history['forgot_pwd_at'];
-                    }
-                }
-            }
-        }
-
-        return $data;
+        return $histories;
     }
 }
