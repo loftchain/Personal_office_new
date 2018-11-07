3db7177a0ec99f5391c54776e203b7e3ce257285
Alexander <meeek196@gmail.com>
2018-11-07 11:04:25 +0300

    Small fixes on the ico page

--- resources/views/home/ico/raised.blade.php
+++ resources/views/home/ico/raised.blade.php
@@ -2,7 +2,7 @@
     <div class="basicBlock">
         <div class="basicBlock__content">
             <div class="basicBlock__title">{!! trans('home/home.totally_raised') !!}</div>
-            <div class="percentBar" data-percent="{{ number_format($data['totalUSDCollected'] / (env('ICO_HARD_CAP') / 100), 0, '.', ' ') }}"> <span class="percentBar__fill"></span><span class="percentBar__marker"></span><span class="percentBar__number"> </span></div>
+            <div class="percentBar" data-percent="{{ number_format($data['totalUSDCollected'] / (env('ICO_HARD_CAP') / 100), 0, '.', ' ') }}"> <span class="percentBar__fill"></span><span class="percentBar__marker" style="{{ number_format($data['totalUSDCollected'] / (env('ICO_HARD_CAP') / 100), 0, '.', ' ') == '0' ? 'margin-left: 0px' : '' }}"></span><span class="percentBar__number"> </span></div>
             <div class="raisedSlider">
                 <div class="owl-carousel owl-theme">
                     <div class="raisedSlider__item"><img class="raisedSlider__itemImage" src="img/eth.svg" alt="eth"><span class="raisedSlider__itemValue">{{ number_format($data['ethCurrentAmount']['currency'], 2, '.', ' ') }}</span></div>
