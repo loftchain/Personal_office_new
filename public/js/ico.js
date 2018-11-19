660d217ae8a352c45461e5a8f48e930adfb3cdc4
Alexander <meeek196@gmail.com>
2018-11-19 12:46:42 +0300

    fixes chart

--- public/js/ico.js
+++ public/js/ico.js
@@ -108,136 +108,136 @@ var buyTokensApp = new Vue({
 
 
 
-  var ctx = $("#tokenChart");
-  var randomScalingFactor = function () {
-    return Math.round(Math.random() * 100);
-  };
-
-  var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      datasets: [{
-        data: [
-          68,
-          15,
-          10,
-          7
-        ],
-        backgroundColor: [
-          '#FFEE6F',
-          '#F71546',
-          '#6C6C6C',
-          '#CFCFCF',
-        ],
-        label: 'Dataset 1'
-      }],
-      labels: [
-        '68% Token Sale',
-        '15% Team',
-        '10% Advisors and Partners',
-        '7% Bounties and Airdrops',
-
-      ]
-    },
-    options: {
-      responsive: true,
-      cutoutPercentage: 92,
-
-      layout: {
-        padding: {
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0
-        }
-      },
-
-
-
-      title: {
-        display: false,
-        text: 'Chart.js Doughnut Chart'
-      },
-      animation: {
-        animateScale: true,
-        animateRotate: true
-      },
-
-      legend: {
-        display: false,
-      },
-
-      legendCallback: function (chart) {
-        var legendHtml = [];
-        legendHtml.push('<ul>');
-        var item = chart.data.datasets[0];
-        for (var i = 0; i < item.data.length; i++) {
-          legendHtml.push('<li>');
-          legendHtml.push('<span class="chart-legend" style="background-color:' + item.backgroundColor[i] + '"></span>');
-          legendHtml.push('<span class="chart-legend-label-text">' + chart.data.labels[i] + '</span>');
-          legendHtml.push('</li>');
-        }
-
-        legendHtml.push('</ul>');
-        return legendHtml.join("");
-      },
-
-    }
-
-  });
-
-
-
-  $('#js-legend').html(myDoughnutChart.generateLegend());
-
-  $('.percentBar').each(function (key, elem) {
-    var percent = $(elem).data('percent');
-    $(elem).find('.percentBar__fill').css('width', percent + '%');
-    $(elem).find('.percentBar__marker').css('left', percent + '%');
-    $(elem).find('.percentBar__number').text(percent + '%');
-    if (percent < 50) {
-      $(elem).find('.percentBar__number').css('color', '#BDBDBD');
-    }
-
-  });
-
-  $('.owl-carousel').owlCarousel({
-    loop: false,
-    dots: false,
-    nav: true,
-    autoWidth: true,
-    items: 3,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
-    }
-  });
-
-
-
-  var toggles = document.querySelectorAll(".cmn-toggle-switch");
-
-  for (var i = toggles.length - 1; i >= 0; i--) {
-    var toggle = toggles[i];
-    toggleHandler(toggle);
-  };
-
-  function toggleHandler(toggle) {
-    toggle.addEventListener("click", function (e) {
-      e.preventDefault();
-      $('.jsSidebar').toggleClass('sidebar--mobileVisible');
-      $('.jsWorkArea').toggleClass('workArea--shifted');
-      (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
-    });
-  }
+  // var ctx = $("#tokenChart");
+  // var randomScalingFactor = function () {
+  //   return Math.round(Math.random() * 100);
+  // };
+  //
+  // var myDoughnutChart = new Chart(ctx, {
+  //   type: 'doughnut',
+  //   data: {
+  //     datasets: [{
+  //       data: [
+  //         68,
+  //         15,
+  //         10,
+  //         7
+  //       ],
+  //       backgroundColor: [
+  //         '#FFEE6F',
+  //         '#F71546',
+  //         '#6C6C6C',
+  //         '#CFCFCF',
+  //       ],
+  //       label: 'Dataset 1'
+  //     }],
+  //     labels: [
+  //       '68% Token Sale',
+  //       '15% Team',
+  //       '10% Advisors and Partners',
+  //       '7% Bounties and Airdrops',
+  //
+  //     ]
+  //   },
+  //   options: {
+  //     responsive: true,
+  //     cutoutPercentage: 92,
+  //
+  //     layout: {
+  //       padding: {
+  //         left: 0,
+  //         right: 0,
+  //         top: 0,
+  //         bottom: 0
+  //       }
+  //     },
+  //
+  //
+  //
+  //     title: {
+  //       display: false,
+  //       text: 'Chart.js Doughnut Chart'
+  //     },
+  //     animation: {
+  //       animateScale: true,
+  //       animateRotate: true
+  //     },
+  //
+  //     legend: {
+  //       display: false,
+  //     },
+  //
+  //     legendCallback: function (chart) {
+  //       var legendHtml = [];
+  //       legendHtml.push('<ul>');
+  //       var item = chart.data.datasets[0];
+  //       for (var i = 0; i < item.data.length; i++) {
+  //         legendHtml.push('<li>');
+  //         legendHtml.push('<span class="chart-legend" style="background-color:' + item.backgroundColor[i] + '"></span>');
+  //         legendHtml.push('<span class="chart-legend-label-text">' + chart.data.labels[i] + '</span>');
+  //         legendHtml.push('</li>');
+  //       }
+  //
+  //       legendHtml.push('</ul>');
+  //       return legendHtml.join("");
+  //     },
+  //
+  //   }
+  //
+  // });
+  //
+  //
+  //
+  // $('#js-legend').html(myDoughnutChart.generateLegend());
+  //
+  // $('.percentBar').each(function (key, elem) {
+  //   var percent = $(elem).data('percent');
+  //   $(elem).find('.percentBar__fill').css('width', percent + '%');
+  //   $(elem).find('.percentBar__marker').css('left', percent + '%');
+  //   $(elem).find('.percentBar__number').text(percent + '%');
+  //   if (percent < 50) {
+  //     $(elem).find('.percentBar__number').css('color', '#BDBDBD');
+  //   }
+  //
+  // });
+  //
+  // $('.owl-carousel').owlCarousel({
+  //   loop: false,
+  //   dots: false,
+  //   nav: true,
+  //   autoWidth: true,
+  //   items: 3,
+  //   navText: ['', ''],
+  //   responsive: {
+  //     0: {
+  //       items: 1
+  //     },
+  //     600: {
+  //       items: 2
+  //     },
+  //     1000: {
+  //       items: 3
+  //     }
+  //   }
+  // });
+  //
+  //
+  //
+  // var toggles = document.querySelectorAll(".cmn-toggle-switch");
+  //
+  // for (var i = toggles.length - 1; i >= 0; i--) {
+  //   var toggle = toggles[i];
+  //   toggleHandler(toggle);
+  // };
+  //
+  // function toggleHandler(toggle) {
+  //   toggle.addEventListener("click", function (e) {
+  //     e.preventDefault();
+  //     $('.jsSidebar').toggleClass('sidebar--mobileVisible');
+  //     $('.jsWorkArea').toggleClass('workArea--shifted');
+  //     (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
+  //   });
+  // }
 
 })();
 
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -8,7 +8,6 @@
 <script src="{{ asset('js/owl-carousel/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('js/fancybox/jquery.fancybox.min.js') }}"></script>
 <script src='https://www.google.com/recaptcha/api.js'></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
 <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 {{--<script src="https://cdn.ethers.io/scripts/ethers-v3.min.js" charset="utf-8" type="text/javascript"></script>--}}
@@ -20,4 +19,6 @@
 
 @if(isset($data) && \Illuminate\Support\Facades\Route::current()->getName() == 'home.index')
     @include('_js.js_widget')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+    @include('_js.js_chart')
 @endif
