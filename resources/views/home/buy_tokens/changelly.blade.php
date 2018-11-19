6c7b0c88081ea1ade5ab6032f04669c80e9c31f0
Nikita <Nkmelnikov@hotmail.com>
2018-11-19 18:13:02 +0300

    changelly height

--- resources/views/home/buy_tokens/changelly.blade.php
+++ resources/views/home/buy_tokens/changelly.blade.php
@@ -2,7 +2,7 @@
     <div class="basicBlock basicBlock--single">
         <div class="basicBlock__content">
             <iframe src="https://changelly.com/widget/v1?auth=email&from=USD&to=ETH&merchant_id=5vwd2ol8nmd39cmr&address=&amount=0&ref_id=5vwd2ol8nmd39cmr&color=ffcd17"
-                    width="600" height="500" class="changelly" scrolling="no" style="overflow-y: hidden; border: none; margin-left: 110px">
+                    width="600" height="400" class="changelly" scrolling="no" style="overflow-y: hidden; border: none; margin-left: 110px">
                 Can't load
                 widget
             </iframe>
