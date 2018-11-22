a47692c54712fa316d4788130c773ec56fbd9c43
Alexander <meeek196@gmail.com>
2018-11-22 23:25:36 +0300

    optimized getting investor referrals

--- resources/views/home/referral/table.blade.php
+++ resources/views/home/referral/table.blade.php
@@ -4,19 +4,17 @@
             <th class="referral__cell referral__cell--person referral__cell--th">{!! trans('home/referrals.tableRef') !!}</th>
             <th class="referral__cell referral__cell--bonus referral__cell--th">{!! trans('home/referrals.tableBonus') !!}</th>
         </tr>
-        @if(isset($referrals['stat']))
-            @foreach($referrals['stat'] as $key => $referral)
-                <tr>
-                    <td class="referral__cell referral__cell--person">{{ $key }}</td>
-                    <td class="referral__cell referral__cell--bonus">{{ isset($referral['token_sum']) ? $referral['token_sum'] : 0}}</td>
-                </tr>
-            @endforeach
 
-        @else
+        @forelse($referrals as $referral)
+            <tr>
+                <td class="referral__cell referral__cell--person">{{ $referral->email }}</td>
+                <td class="referral__cell referral__cell--bonus">{{ $referral->transactions->sum('amount_tokens') * 0.1 }}</td>
+            </tr>
+        @empty
             <tr>
                 <td class="referral__cell referral__cell--person">No</td>
                 <td class="referral__cell referral__cell--bonus">0</td>
             </tr>
-        @endif
+        @endforelse
     </table>
 </div>
