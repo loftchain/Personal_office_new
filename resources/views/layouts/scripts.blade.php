36daf86eb4c05078612d2dbdf6ba7d80c6d2fb93
Alexander <meeek196@gmail.com>
2018-11-23 21:41:12 +0300

    some fixes

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -10,7 +10,11 @@
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
-<script src="{{ asset('js/ethers/dist/ethers.js') }}"></script>
+{{--<script src="{{ asset('js/ethers/dist/ethers.js') }}"></script>--}}
+<script charset="utf-8"
+        src="https://cdn.ethers.io/scripts/ethers-v4.min.js"
+        type="text/javascript">
+</script>
 <script src="{{ asset('js/ico.js') }}"></script>
 @include('_js.js_custom_validation')
 @include('_js.js_main_menu')
