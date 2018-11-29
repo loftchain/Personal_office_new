8658bb614de7e8436d6d4c1ec9a658f1074b9962
Sergej <serezha11shumov@mail.ru>
2018-11-29 15:58:36 +0300

    add target-blank to footer links

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<p class="page-footer__text">Developed by <a class="page-footer__link" href="https://loftchain.io/">Loftchain</a></p>
+<p class="page-footer__text">Developed by <a class="page-footer__link" href="https://loftchain.io/" target="_blank">Loftchain</a></p>
 <ul class="page-footer__list">
-    <li class="page-footer__list-item"><a class="page-footer__link" href="https://mitoshi.io/">ICO site</a></li>
+    <li class="page-footer__list-item"><a class="page-footer__link" href="https://mitoshi.io/" target="_blank">ICO site</a></li>
     <li class="page-footer__list-item"><a class="page-footer__link" href="{{ asset('/whitepaper.pdf') }}" target="_blank">White Paper</a></li>
 </ul>
