fa66d83a3ebff421d686c48ba68e46aa5239c852
Nikita <Nkmelnikov@hotmail.com>
2018-11-29 16:57:17 +0300

    configurable footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="page-footer__text">Developed by <a class="page-footer__link" href="https://loftchain.io/" target="_blank">Loftchain</a></p>
 <ul class="page-footer__list">
-    <li class="page-footer__list-item"><a class="page-footer__link" href="https://mitoshi.io/" target="_blank">ICO site</a></li>
-    <li class="page-footer__list-item"><a class="page-footer__link" href="{{ asset('/whitepaper.pdf') }}" target="_blank">White Paper</a></li>
+    <li class="page-footer__list-item"><a class="page-footer__link" href="{{ env('MAIN_SITE_URL') }}" target="_blank">ICO site</a></li>
+    <li class="page-footer__list-item"><a class="page-footer__link" href="{{ asset(env('WP_URL')) }}" target="_blank">White Paper</a></li>
 </ul>
