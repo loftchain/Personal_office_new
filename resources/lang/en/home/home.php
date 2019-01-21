791fd42d7c3cb5a08c958e6a133115c5dfaf9276
Alexander <meeek196@gmail.com>
2019-01-21 11:29:27 +0300

    update round name & update widget name

--- resources/lang/en/home/home.php
+++ resources/lang/en/home/home.php
@@ -5,7 +5,7 @@ return [
     'timer_day' => 'days',
     'timer_hour' => 'hours',
     'timer_min' => 'min',
-    'totally_raised' => 'TOTALLY RAISED',
+    'totally_raised' => 'TOTAL RAISED',
     'deposit_bonus' => 'DEPOSIT AMOUNT BONUSES',
     'token_distribution' => 'Token Distribution',
     'round_info' => 'Round info',
